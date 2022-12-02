10f84cf577d6a4bf32090223b7f292dcc1a89b20
Tomas Strand <tomas@fik1.net>
2022-12-02 11:09:03 +0200

    format day1

--- src/day1.rs
+++ src/day1.rs
@@ -6,13 +6,13 @@ pub fn run() -> io::Result<()> {
 
     // Reads one line as str
     let lines = helpers::read_file_to_vec::<String>("inputs/day1.txt");
-    let cc:Vec<i32> = lines.into_iter().map(|x| {
-        match x.parse::<i32>() {
-            Ok(n) =>  n,
+    let cc: Vec<i32> = lines
+        .into_iter()
+        .map(|x| match x.parse::<i32>() {
+            Ok(n) => n,
             _ => 0,
-        }
-        
-    }).collect();
+        })
+        .collect();
 
     let elfs: Vec<Vec<i32>> = cc.into_iter().fold(Vec::new(), |mut acc, x| {
         if x == 0 || acc.is_empty() {
@@ -23,12 +23,14 @@ pub fn run() -> io::Result<()> {
         }
         acc
     });
-    let mut sums:Vec<i32> = elfs.iter().map(|x| x.iter().fold(0, |acc, x| acc+x)).collect();
+    let mut sums: Vec<i32> = elfs
+        .iter()
+        .map(|x| x.iter().fold(0, |acc, x| acc + x))
+        .collect();
     println!("Part1: {:?}", sums.iter().max());
 
-
-    sums.sort_by(|a,b| b.cmp(a) );
-    let top3:i32 = sums.iter().take(3).sum();
+    sums.sort_by(|a, b| b.cmp(a));
+    let top3: i32 = sums.iter().take(3).sum();
     println!("Part2: {:?}", top3);
 
     Ok(())
