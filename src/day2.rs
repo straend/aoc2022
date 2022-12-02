4b5b10f74d32453949418f0034f6e90b63d578cc
Tomas Strand <tomas@fik1.net>
2022-12-02 14:47:20 +0200

    Remove useless split

--- src/day2.rs
+++ src/day2.rs
@@ -10,7 +10,7 @@ pub fn run(bench: bool) -> io::Result<(u128, u128, u128)> {
     let start = Instant::now();
     
     let lines = helpers::read_file_to_vec::<String>("inputs/day2.txt");
-    let lines: Vec<(&str, &str)> = lines.iter().map(|x| { x.split_once(" ").unwrap() }).collect();
+    //let lines: Vec<(&str, &str)> = lines.iter().map(|x| { x.split_once(" ").unwrap() }).collect();
     
     let t_input = start.elapsed().as_micros();
     
@@ -25,25 +25,25 @@ pub fn run(bench: bool) -> io::Result<(u128, u128, u128)> {
     
     // Not so elegant, bore of a brute force mapping, but it works
     let sum:i32 = lines.iter().map(|x| {
-        match x {
+        match x as &str {
             // A X Rock
             // B Y Paper
             // C Z Scissor
             // Rock
-            ("A", "X") => 1+3,
-            ("A", "Y") => 2+6,
-            ("A", "Z") => 3+0,
+            "A X" => 1+3,
+            "A Y" => 2+6,
+            "A Z" => 3+0,
 
             // Paper
-            ("B", "X") => 1+0,
-            ("B", "Y") => 2+3,
-            ("B", "Z") => 3+6,
+            "B X" => 1+0,
+            "B Y" => 2+3,
+            "B Z" => 3+6,
 
             // Scisssor
-            ("C", "X") => 1+6,
-            ("C", "Y") => 2+0,
-            ("C", "Z") => 3+3,
-            (_, _) => 0,
+            "C X" => 1+6,
+            "C Y" => 2+0,
+            "C Z" => 3+3,
+            _ => 0,
         }
     }).fold(0, |acc, x| acc+ x);
     
@@ -54,27 +54,27 @@ pub fn run(bench: bool) -> io::Result<(u128, u128, u128)> {
     let start = Instant::now();
 
     let sum:i32 = lines.iter().map(|x| {
-        match x {
+        match x as &str{
             // A X Rock
             // B Y Paper
             // C Z Scissor
             // Second column 
             //  X Loose, Y Draw, Z Win
             // Rock
-            ("A", "X") => 3+0,
-            ("A", "Y") => 1+3,
-            ("A", "Z") => 2+6,
+            "A X" => 3+0,
+            "A Y" => 1+3,
+            "A Z" => 2+6,
 
             // Paper
-            ("B", "X") => 1+0,
-            ("B", "Y") => 2+3,
-            ("B", "Z") => 3+6,
+            "B X" => 1+0,
+            "B Y" => 2+3,
+            "B Z" => 3+6,
 
             // Scisssor
-            ("C", "X") => 2+0,
-            ("C", "Y") => 3+3,
-            ("C", "Z") => 1+6,
-            (_, _) => 0,
+            "C X" => 2+0,
+            "C Y" => 3+3,
+            "C Z" => 1+6,
+             _ => 0,
         }
     }).fold(0, |acc, x| acc+ x);
 
